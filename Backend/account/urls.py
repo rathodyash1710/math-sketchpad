578eebaefdfdf61031f2fdddfbe83dbcbccccdb9
rathodyash1710 <yashrathod1710@gmail.com>
2024-10-12 14:34:16 +0530

    Your commit message here

--- Backend/account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import RegisterView, LoginView, LogoutView
-
-urlpatterns = [
-    path('signup/', RegisterView.as_view(), name='signup'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-]
--- Frontend/src/constants.js
+++
@@ -1,5 +0,0 @@
-const ACCESS_TOKEN = () => window.localStorage.getItem('token');
-const SET_ACCESS_TOKEN = (token) => window.localStorage.setItem('token', token);
-const CLEAR_ACCESS_TOKEN = () => window.localStorage.clear();
-
-export { ACCESS_TOKEN, SET_ACCESS_TOKEN, CLEAR_ACCESS_TOKEN };
